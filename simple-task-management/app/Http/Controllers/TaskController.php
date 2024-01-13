a9008300cf6480b29487f817adc4f5c6d15c9160
Michael George <111292703+mGeorge90@users.noreply.github.com>
2024-01-13 23:47:56 +0200

    add task list

--- simple-task-management/app/Http/Controllers/TaskController.php
+++ simple-task-management/app/Http/Controllers/TaskController.php
@@ -3,10 +3,16 @@
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateTaskRequest;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 
 class TaskController extends Controller
 {
+    public function index()
+    {
+        $tasks = Task::with(['assignedTo', 'assignedBy'])->get();
+        return response()->json(TaskResource::collection($tasks), 200);
+    }
     public function store(CreateTaskRequest $request)
     {
         $task = Task::create($request->only(['title', 'description', 'assigned_to_id', 'assigned_by_id']));
