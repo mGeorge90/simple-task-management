aec644bf6614fd48909f4ddfca57b81ad4240d95
Michael George <111292703+mGeorge90@users.noreply.github.com>
2024-01-13 22:25:54 +0200

    publish sial for docker steps

--- simple-task-management/app/Providers/AuthServiceProvider.php
+++ simple-task-management/app/Providers/AuthServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -19,10 +19,8 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any authentication / authorization services.
      */
-    public function boot()
+    public function boot(): void
     {
-        $this->registerPolicies();
-
-        Passport::routes();
+        //
     }
 }
